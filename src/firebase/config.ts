156124378bf7bf8c318236fd096e9627befcb6cf
boba Ahmed <bobaahmed811@gmail.com>
2025-12-25 23:03:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1391317803-d393f",
+  "appId": "1:259088451308:web:fd708dc068d59d431807c9",
+  "apiKey": "AIzaSyASKFdYPoeHuENGY9UrS0aFefBa6PuHxmw",
+  "authDomain": "studio-1391317803-d393f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "259088451308"
+};
