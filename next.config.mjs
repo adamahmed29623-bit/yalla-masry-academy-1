337d81d2dcd4e847b967fd406954ac57719159f4
adamahmed29623-bit <adamahmed29623@gmail.com>
2026-01-09 16:40:14 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // هذا سيحول الأكاديمية إلى نظام ثابت تماماً ويتخطى أخطاء الذاكرة
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
-  // إضافة هذا السطر لتجاوز أخطاء الـ Prerendering
-  output: 'standalone', 
+  images: { unoptimized: true }, // ضروري عند استخدام نظام export
 };
 
 export default nextConfig;
