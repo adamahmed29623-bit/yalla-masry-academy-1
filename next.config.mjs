587ae5607c64d10dc0e600ea5b925078074a5ac6
adamahmed29623-bit <adamahmed29623@gmail.com>
2026-01-09 02:27:46 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // سيسمح هذا للأكاديمية بالانتشار حتى لو وجد أخطاء في "الأنواع"
-    // لن يتوقف البناء بعد الآن بسبب السطر 17
+    // سيسمح هذا للأكاديمية بالانتشار وتجاهل أخطاء "الأنواع" تماماً
     ignoreBuildErrors: true,
   },
   eslint: {
     // لتجنب أي تعطل بسبب قواعد التنسيق أثناء النشر
     ignoreDuringBuilds: true,
   },
-}
+};
 
-module.exports = nextConfig;
+export default nextConfig;
