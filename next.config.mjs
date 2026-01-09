168f157b895956e77f3c50233ced0a763df49190
adamahmed29623-bit <adamahmed29623@gmail.com>
2026-01-09 14:55:59 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,5 +2,8 @@
 const nextConfig = {
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
+  // إضافة هذا السطر لتجاوز أخطاء الـ Prerendering
+  output: 'standalone', 
 };
+
 export default nextConfig;
