1c4c2ffbdf188f3e64c7eeccb53da5b22bb80388
adamahmed29623-bit <adamahmed29623@gmail.com>
2026-01-11 18:42:02 +0200

    Update i18n-config.ts

--- src/i18n-config.ts
+++ src/i18n-config.ts
@@ -1,6 +1,7 @@
 export const i18n = {
-    defaultLocale: 'en',
-    locales: ['en', 'ar', 'fr'],
+  defaultLocale: 'ar',
+  // أضفنا 'ur' لتكتمل المجموعة الدولية
+  locales: ['ar', 'en', 'fr', 'es', 'de', 'it', 'ru', 'zh', 'ja', 'tr', 'ur'],
 } as const;
 
 export type Locale = (typeof i18n)['locales'][number];
