79eec417d6e4b46070fb6cc5193cafb1939a88b6
adamahmed29623-bit <adamahmed29623@gmail.com>
2026-01-14 00:44:55 +0200

    Update page.tsx

--- src/app/project/[id]/firestore/page.tsx
+++ src/app/project/[id]/firestore/page.tsx
@@ -1,7 +1,5 @@
-export const dynamic = 'force-static';
-export function generateStaticParams() {
-  return [];
-}
+"use client";
+export const dynamic = 'force-dynamic';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import RulesEditor from "@/components/firestore/rules-editor";
